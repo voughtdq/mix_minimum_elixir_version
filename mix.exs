a321d0720da0c42875f5085473de39bcbde4c5d7
Douglas Vought <doug@vought.co>
2024-07-16 23:38:13 -0400

    Tweak mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule MixMinimumElixirVersion.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -17,7 +19,19 @@ defmodule MixMinimumElixirVersion.MixProject do
     [
       {:req, "~> 0.5.0"},
       {:jason, "~> 1.4"},
-      {:ex_doc, "~> 0.24", only: :docs, runtime: false}
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "Mix task to find the minimum Elixir version requirement for a particular project."
+  end
+
+  defp package do
+    [
+      maintainers: ["Douglas Vought"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/voughtdq/mix_minimum_elixir_version"}
     ]
   end
 end
